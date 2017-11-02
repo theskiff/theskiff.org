b4d651081b40717500ed0f999ffb857fd9a53df7
Jonathan Markwell <jonathan.markwell@gmail.com>
2017-11-02 09:12:43 +0000

    more than should ever be in one commit

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,45 @@
 // This is where it all goes :)
+window._drip_pro = new DripPro
+
+window.drip_plinko = function(drip, page) {
+  var response = {}
+  if (drip.is_anon) {
+
+  } else {
+    if (drip.custom_fields()['plan_id'] == undefined) {
+    } else {
+      // alert(drip.custom_fields()['plan_id'])
+    }
+  }
+
+  return response
+}
+
+
+jQuery(function(){
+  window._drip_pro.init({
+    debug: true,
+    template_path: '/ad-examples/',
+    survey_trigger: function() {
+      return false // If a question is available, should the survey widget be triggered?
+    },
+    can_display: function(intent, options) {
+      return false // Given the "intent" (like modal), should we show it on this page? Useful for checking to see if you're on the checkout page, etc. and forcing DPT offers to not show.
+    },
+    pre_init: function() {
+    },
+    post_drip_response: function(payload) {
+    },
+    score: function(drip) {
+    },
+    increment_score: function(category, article_id) {
+    },
+    content_leaderboard: function() {
+
+    },
+    track: function() {
+    },
+    renderers: {
+    }
+  })
+})
